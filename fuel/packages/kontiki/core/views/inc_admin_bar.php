b9841a62147716919c0ab0b9fc22cccf7256b7fb
有松 <arimatsu@jidaikobo.com>
2014-10-03 21:36:55 +0900

    管理バーのコンテクストメニューから各種コントロールを分離

--- fuel/packages/kontiki/core/views/inc_admin_bar.php
+++ fuel/packages/kontiki/core/views/inc_admin_bar.php
@@ -7,12 +7,12 @@ if($is_user_logged_in):
 	
 	$html = '';
 	$html.= '<div id="admin_bar">';
-	$html.= '<a href="#anchor_admin_bar" class="skip show-if-focus" tabindex="1">管理バーに移動</a>';//移動用のアンカーだが、どのような形がよいのか要検討。
-	$html.= '<a id="anchor_admin_bar" tabindex="0"></a>';
+	$html.= '<a href="#anchor_admin_bar" class="skip show-if-focus" tabindex="1">管理バーに移動</a>';
+	$html.= '<a id="anchor_admin_bar" class="skip" tabindex="0">管理バー</a>';
 	//controller menu
 	$controller4menu = $get_controllers();
 	if($controller4menu):
-		$html.= '<div id="admin_bar_controller">';
+		$html.= '<div id="admin_bar_controller" class="menulist">';
 		$html.= '<h2><a href="javascript:void(0);" class="listOpen">コントローラメニュー</a></h2>';
 		$html.= '<div class="admin_bar_sub">';
 		$html.= '<ul>';
@@ -22,18 +22,18 @@ if($is_user_logged_in):
 		endforeach;
 		$html.= '</ul>';
 		$html.= '</div><!-- /.admin_bar_sub -->';
-		$html.= '</div>';
+		$html.= '</div><!-- /.menulist -->';
 	endif;
 
 	//context menu
 	$actions = $get_actionset($controller, $item);
-	if($actions['index'] || $actions['control']):
-		$html.= '<div id="admin_bar_context">';
-		$html.= '<h2><a href="javascript:void(0);" class="listOpen">コンテクストメニュー</a></h2>';
-		$html.= '<div class="admin_bar_sub">';
-	endif;
 	if($actions['index']):
-		$html.= '<h3>各種インデクス</h3>';
+		$html.= '<div id="admin_bar_context" class="menulist">';
+		$html.= '<h2><a href="javascript:void(0);" class="listOpen">コンテクストメニュー</a></h2>';
+//	endif;
+//	if($actions['index']):
+		$html.= '<div class="admin_bar_sub">';
+//		$html.= '<h3>各種インデクス</h3>';
 		$html.= '<ul>';
 		foreach($actions['index'] as $url => $v):
 			if( ! $url) continue;
@@ -42,9 +42,17 @@ if($is_user_logged_in):
 			$html.= "<li><a href=\"{$home_uri}{$url}\"{$script}>{$v['menu_str']}</a></li>";
 		endforeach;
 		$html.= '</ul>';
+//	endif;
+//各種コントロール、元の位置
+//	if($actions['index'] || $actions['control']):
+		$html.= '</div><!-- /.admin_bar_sub -->';
+		$html.= '</div>';
 	endif;
+	//
 	if($actions['control']):
-		$html.= '<h3>各種コントロール</h3>';
+		$html.='<div id="admin_bar_context_control" class="openlist">';
+//		$html.= '<h2><a href="javascript:void(0);" class="listOpen">各種コントロール</a></h2>';
+//		$html.= '<div class="admin_bar_sub">';
 		$html.= '<ul>';
 		foreach($actions['control'] as $url => $v):
 			if( ! $url) continue;
@@ -53,15 +61,13 @@ if($is_user_logged_in):
 			$html.= "<li><a href=\"{$home_uri}{$url}\"{$script}>{$v['menu_str']}</a></li>";
 		endforeach;
 		$html.= '</ul>';
-	endif;
-	if($actions['index'] || $actions['control']):
-		$html.= '</div><!-- /.admin_bar_sub -->';
+//		$html.= '</div><!-- /.admin_bar_sub -->';
 		$html.= '</div>';
 	endif;
 
 	//user menu
 	$html.= '<div id="admin_bar_user">';
-	$html.= '<h2><a href="javascript:void(0);" class="listOpen">'.\User\Controller_User::$userinfo['user_name'].'</a></h2>';
+	$html.= '<h2>'.\User\Controller_User::$userinfo['user_name'].'</h2>';
 	$html.= '<ul>';
 	$html.= "<li><a href=\"{$home_uri}user/logout\">ログアウト</a></li>";
 	$html.= '</ul>';
