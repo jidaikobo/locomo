195826bd1b1dce3ca2cb8b990231a0b9cfb92611
shibata <shibata@jidaikobo.com>
2014-10-29 16:10:11 +0900

    bulk_definitionを削除し、form_definitionに統一。

--- fuel/packages/locomo/core/modules/user/classes/model/usergroup.php
+++ fuel/packages/locomo/core/modules/user/classes/model/usergroup.php
@@ -14,18 +14,7 @@ class Model_Usergroup extends \Locomo\Model_Base
 		'deleted_at',
 	);
 
-	protected static $_many_many = array(
-		'user' => array(
-			'key_from' => 'id',
-			'key_through_from' => 'group_id',
-			'table_through' => 'usergroups_r',
-			'key_through_to' => 'user_id',
-			'model_to' => '\User\Model_User',
-			'key_to' => 'id',
-			'cascade_save' => false,
-			'cascade_delete' => false,
-		)
-	);
+	protected static $_many_many = array();
 
 	protected static $_soft_delete = array(
 		'deleted_field'   => 'deleted_at',
@@ -41,7 +30,7 @@ class Model_Usergroup extends \Locomo\Model_Base
 			'label'           => 'name',
 			'order_field'     => 'order',
 			'template'        => '',//use bulk
-			'form_definition' => 'bulk_definition',
+			'form_definition' => 'form_definition',
 			'option' => array(
 				'select' => array('name'),
 				'where'  => array(
@@ -77,9 +66,17 @@ class Model_Usergroup extends \Locomo\Model_Base
 	 *
 	 * @return  obj
 	 */
-	public static function form_definition($factory, $obj = null, $id = '')
+	public static function form_definition($factory, $obj = null)
 	{
 		$form = \Fieldset::forge($factory, \Config::get('form'));
+
+		//id
+		$form->add(
+			'id',
+			'ID',
+			array('type' => 'text', 'disabled' => 'disabled', 'size' => 2)
+		)
+		->set_value(@$obj->id);
 
 		//name
 		$form->add(
@@ -90,7 +87,7 @@ class Model_Usergroup extends \Locomo\Model_Base
 			->set_value(@$obj->name)
 			->add_rule('required')
 			->add_rule('max_length', 50)
-			->add_rule('unique', "usergroups.name.{$id}");
+			->add_rule('unique', "usergroups.name.".@$obj->id);
 
 		//description
 		$form->add(
@@ -114,62 +111,11 @@ class Model_Usergroup extends \Locomo\Model_Base
 		$form->add(
 				'is_available',
 				'使用中',
-				array('type' => 'checkbox')
+				array('type' => 'select', 'options' => array('0' => '未使用', '1' => '使用中'), 'default' => 0)
 			)
 			->set_value(@$obj->is_available);
 
 		return $form;
-	}
-
-	public static function bulk_definition($factory, $obj = null, $id = '')
-	{
-		$form = \Fieldset::forge($factory, \Config::get('form'));
-
-		//id
-		$form->add(
-			'id',
-			'ID',
-			array('type' => 'text', 'disabled' => 'disabled', 'size' => 2)
-		)
-		->set_value(@$obj->id);
-
-		//user_name
-		$form->add(
-				'name',
-				'ユーザグループ名',
-				array('type' => 'text', 'size' => 20)
-			)
-			->set_value(@$obj->name)
-			//->add_rule('required')
-			->add_rule('max_length', 50)
-			->add_rule('unique', "usergroups.name.{$obj->id}");
-
-		//display_name
-		$form->add(
-				'description',
-				'説明',
-				array('type' => 'text', 'size' => 20)
-			)
-			->set_value(@$obj->description)
-			->add_rule('max_length', 255);
-
-		$form->add(
-				'order',
-				'表示順',
-				array('type' => 'text', 'size' => 5)
-			)
-			->set_value(@$obj->order)
-			->add_rule('valid_string', array('numeric'));
-
-		$form->add(
-				'is_available',
-				'使用中',
-				array('type' => 'select', 'options' => array('0' => '未使用', '1' => '使用中'), 'default' => 0)
-			)
-			->set_value($obj->is_available, true);
-
-		return $form;
-
 	}
 
 }
