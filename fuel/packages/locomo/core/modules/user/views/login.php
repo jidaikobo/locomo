3b6fa71b4a9025fbc25860ba1672b7c462bec583
有松 <arimatsu@jidaikobo.com>
2014-11-03 14:44:07 +0900

    ログインフォーム周りでplaceholderやlabelの実験中。

--- fuel/packages/locomo/core/modules/user/views/login.php
+++ fuel/packages/locomo/core/modules/user/views/login.php
@@ -1,36 +1,19 @@
 <?php echo render('inc_header'); ?>
 
-<script type="text/javascript">
-<!--thx WordPress-->
-function locomo_attempt_focus(){
-	setTimeout(
-		function(){
-			try{
-				account = document.getElementById('form_account');
-				account.focus();
-				account.select();
-			} catch(e){}
-		}, 200
-	);
-}
-locomo_attempt_focus();
-</script>
-
-
 <h2><img src="/content/fetch_view/images/parts/logo.png" alt=""><?php echo $site_title ;?></h2>
 
 <?php echo \Form::open(array('action' => \Uri::base(false).'user/login/','class' => 'login')); ?>
 
 <!--ユーザ名かメールアドレス-->
 <div class="form_group">
-	<?php echo \Form::label('アカウント', 'account'); ?>
-	<?php echo \Form::input('account', Input::post('account', isset($item) ? $item->account : ''), array('placeholder'=>'ユーザ名かメールアドレスを入力')); ?>
+	<?php echo '<span id="label1">'.\Form::label('アカウント', 'account').'</span>'; ?>
+	<?php echo '<span id="desc1" class="skip">ユーザ名かメールアドレス</span>'.\Form::input('account', Input::post('account', isset($item) ? $item->account : ''), array('placeholder'=>'ユーザ名かメールアドレス','aria-describedby'=>'desc1')); ?>
 </div>
 
 <!--パスワード-->
 <div class="form_group">
-	<?php echo \Form::label('パスワード', 'password'); ?>
-	<?php echo \Form::password('password', Input::post('password', isset($item) ? $item->password : ''), array('placeholder'=>'パスワードを入力')); ?>
+	<?php echo '<span id="label2">'. \Form::label('パスワード', 'password').'</span>'; ?>
+	<?php echo \Form::password('password', Input::post('password', isset($item) ? $item->password : ''), array('placeholder'=>'パスワード')); ?>
 </div>
 
 <?php
