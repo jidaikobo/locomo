2d08aec1854688de2b955c51b05ab9f83501c15b
shibata <shibata@jidaikobo.com>
2014-11-16 20:15:16 +0900

    formのconfigを若干修正

--- fuel/packages/locomo/modules/user/views/edit.php
+++ fuel/packages/locomo/modules/user/views/edit.php
@@ -45,10 +45,7 @@ if( ! \Auth::is_admin()): ?>
 	</tr>
 	<?php endif; ?>
 	</table>
-	<div class="revision_comment">
-		<h3><a href="javascript: void(0);" class="toggle_item disclosure"><?php echo \Form::label('編集履歴用メモ', 'revision_comment'); ?></a></h3>
-	<?php echo \Form::textarea('revision_comment', Input::post('revision_comment', isset($item->comment) ? $item->comment : ''), array('style'=>'width: 100%;','class'=>'hidden_item')); ?>
-	</div>
+	<?php echo render(LOCOMOPATH.'modules/revision/views/inc_revision_memo.php'); ?>
 </div>
 <div class="submit_button">
 	<?php
