5c7a5308d51df4ec89995d736b8510349e27c0b0
shibata <shibata@jidaikobo.com>
2014-11-19 22:26:54 +0900

    assetパスを修正

--- views/inc_admin_bar.php
+++ views/inc_admin_bar.php
@@ -42,7 +42,7 @@ if(\Auth::check()):
 				$optmenu = \Arr::get($actionset, 'option') ? \Actionset::generate_menu_html($actionset['option'], array('class'=>'modal hidden_item boxshadow')) : false ;
 				if($optmenu):
 					$html.= '<div class="admin_module_option">';
-					$html.= "<a href=\"javascript:void(0)\" class=\"modal has_dropdown toggle_item\" title=\"".\Config::get('nicename')."の設定を開く\"><span class=\"adminbar_icon icononly\"><img src=\"".\Uri::base()."content/fetch_view/images/parts/adminbar_icon_module_option.png\" alt=\"".\Config::get('nicename')."の設定\"></span></a>";
+					$html.= "<a href=\"javascript:void(0)\" class=\"modal has_dropdown toggle_item\" title=\"".\Config::get('nicename')."の設定を開く\"><span class=\"adminbar_icon icononly\"><img src=\"".\Uri::base()."content/fetch_view/img/system/adminbar_icon_module_option.png\" alt=\"".\Config::get('nicename')."の設定\"></span></a>";
 					$html.= $optmenu;
 					$html.= '</div><!-- .admin_module_option -->';
 				endif;
@@ -52,7 +52,7 @@ if(\Auth::check()):
 
 		//adminbar_top  -- logo(sitetop), mainmenu, user, option, renderinfo
 		$html.= '<div class="adminbar_top">'; 
-			$html.= "<img src=\"".\Uri::base()."content/fetch_view/images/parts/logo.png\" id=\"adminbar_logo\" alt=\"".\Config::get('site_title')."\" title=\"".\Config::get('site_title')."トップへ\">" ;
+			$html.= "<img src=\"".\Uri::base()."content/fetch_view/img/system/logo.png\" id=\"adminbar_logo\" alt=\"".\Config::get('site_title')."\" title=\"".\Config::get('site_title')."トップへ\">" ;
 			$html.= '<div class="adminbar_main">';
 			$html.= \Config::get('no_home') ? '' : '<span><a href="'.\Uri::base().'" style="color:#fff;">ホーム</a></span>';
 			$html.= '<span><a href="'.\Uri::base().'admin/dashboard/" style="color:#fff;">ダッシュボード</a></span>';
@@ -60,7 +60,7 @@ if(\Auth::check()):
 			//controller menu
 			if($locomo['controllers']):
 				$html.= '<div class="admin_menu">';
-				$html.= '<a href="javascript:void(0);" class="modal has_dropdown toggle_item" title="メニューを開く"><span class="adminbar_icon">'."<img src=\"".\Uri::base()."content/fetch_view/images/parts/adminbar_icon_menu.png\" alt=\"\">".'</span><span class="hide_if_smalldisplay">メニュー</span></a>';
+				$html.= '<a href="javascript:void(0);" class="modal has_dropdown toggle_item" title="メニューを開く"><span class="adminbar_icon">'."<img src=\"".\Uri::base()."content/fetch_view/img/system/adminbar_icon_menu.png\" alt=\"\">".'</span><span class="hide_if_smalldisplay">メニュー</span></a>';
 				// IE8では画像のサイズをCSSで与えた場合、画像の本来のサイズで親要素が描画されてしまうので、明示的なサイズを持った要素で画像を囲む。
 				$html.= '<ul class="modal hidden_item boxshadow">';
 				foreach($locomo['controllers'] as $k => $v):
@@ -81,7 +81,7 @@ if(\Auth::check()):
 		
 			//user menu
 			$html.= '<div class="adminbar_user">';
-			$html.= '<a href="javascript:void(0);" class="modal has_dropdown toggle_item" title="ユーザメニューを開く:'.\Auth::get('display_name').'でログインしています"><span class="adminbar_icon">'."<img src=\"".\Uri::base()."content/fetch_view/images/parts/adminbar_icon_user{$root_prefix}.png\" alt=\"\"></span><span class=\"hide_if_smalldisplay\">".\Auth::get('display_name').'</span></a>';
+			$html.= '<a href="javascript:void(0);" class="modal has_dropdown toggle_item" title="ユーザメニューを開く:'.\Auth::get('display_name').'でログインしています"><span class="adminbar_icon">'."<img src=\"".\Uri::base()."content/fetch_view/img/system/adminbar_icon_user{$root_prefix}.png\" alt=\"\"></span><span class=\"hide_if_smalldisplay\">".\Auth::get('display_name').'</span></a>';
 			$html.= '<ul class="modal hidden_item boxshadow">';
 			$html.= '<li class="show_if_smalldisplay"><span class="label">'.\Auth::get('display_name').'</span></li>';
 			if( ! \Auth::is_admin()):
@@ -94,7 +94,7 @@ if(\Auth::check()):
 			//admin option menu
 			if(\Auth::is_admin()):
 				$html.= '<div class="admin_option">';
-				$html.= "<a href=\"javascript:void(0)\" class=\"modal has_dropdown toggle_item\" title=\"管理者設定を開く\"><span class=\"adminbar_icon icononly\"><img src=\"".\Uri::base()."content/fetch_view/images/parts/adminbar_icon_option.png\" alt=\"管理者設定\"></span></a>";
+				$html.= "<a href=\"javascript:void(0)\" class=\"modal has_dropdown toggle_item\" title=\"管理者設定を開く\"><span class=\"adminbar_icon icononly\"><img src=\"".\Uri::base()."content/fetch_view/img/system/adminbar_icon_option.png\" alt=\"管理者設定\"></span></a>";
 				$html.= '<ul class="modal hidden_item boxshadow">';
 					foreach($locomo['controllers'] as $k => $v):
 						if($v['is_for_admin'] && $v['show_at_menu'])
@@ -109,7 +109,7 @@ if(\Auth::check()):
 
 			//help
 			$html.= '<div class="admin_help">';
-				$html.= '<a href="'.\Uri::base().'help/help/index_admin?searches[mod_or_ctrl]='.$locomo['current']['mod_or_ctrl']['name'].'" title="ヘルプ"><span class="adminbar_icon">'."<img src=\"".\Uri::base()."content/fetch_view/images/parts/adminbar_icon_help.png\" alt=\"ヘルプ\">".'</span></a>';
+				$html.= '<a href="'.\Uri::base().'help/help/index_admin?searches[mod_or_ctrl]='.$locomo['current']['mod_or_ctrl']['name'].'" title="ヘルプ"><span class="adminbar_icon">'."<img src=\"".\Uri::base()."content/fetch_view/img/system/adminbar_icon_help.png\" alt=\"ヘルプ\">".'</span></a>';
 			$html.= '</div><!-- /.admin_help -->';
 			
 			//処理速度
