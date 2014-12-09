848d02588128f107581439541ce41ef0453c89fb
tsukitsume <otegami@tsukitsume.com>
2014-12-10 02:22:58 +0900

    消えていたutil の function を復活

--- classes/util.php
+++ classes/util.php
@@ -71,4 +71,29 @@ class Util
 		$latest_prefix = sprintf($format , $latest_prefix + 1);
 		return $latest_prefix;
 	}
+
+
+	/*
+	 * 年度の機関を between 句で返す
+	 * todo 4月のみ対応しているので、後々変える
+	 * @return where between 句
+	 */
+	public static function get_fiscal($date = null) {
+
+		
+		if (preg_match('/^([0-9]{4})$/', $date)) $date = date('Y-n-d', strtotime($date . '-04-01'));
+		//var_dump(date('Y-n', strtotime($date))); die();
+		!$date and $date = date('Y-m-d');
+
+		if (date('n', strtotime($date)) < 4) {
+			$year = date('Y', strtotime($date)) - 1;
+		} else {
+			$year = date('Y', strtotime($date));
+		}
+
+		return \DB::expr('"' . $year . '-04-01 00:00:00"' . ' and ' . '"' . ($year+1) . '-03-31 23:59:59"');
+	}
+
+
+
 }
