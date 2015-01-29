71962fe81930c23ded2f6eb65445ac815740a59c
shibata <shibata@jidaikobo.com>
2015-01-30 03:47:40 +0900

    エラーページにシステムのメッセージを表示するようにした。

--- views/error.php
+++ views/error.php
@@ -4,8 +4,26 @@
 	<meta charset="utf-8">
 	<title><?php echo $title ?></title>
 	<meta name="robots" content="noindex, follow, noarchive">
+	<style type="text/css">
+	h1
+	{
+		font-size: 130%;
+	}
+
+	.skip
+	{
+		position: absolute !important;
+		left: auto;
+		clip: rect(1px 1px 1px 1px);
+		clip: rect(1px, 1px, 1px, 1px);
+		height: 1px;
+		width: 1px;
+		overflow: hidden;
+	}
+	</style>
 </head>
 <body>
+<?php echo render('inc_messages'); ?>
 <?php echo $content ?>
 </body>
 </html>
