9f6fb01df49f53732ab3de96554523afd11da6a4
shibata <shibata@jidaikobo.com>
2015-02-01 21:44:53 +0900

    ワークフローのテンプレートの微修正。

--- classes/controller/traits/wrkflw.php
+++ classes/controller/traits/wrkflw.php
@@ -49,33 +49,45 @@ trait Controller_Traits_Wrkflw
 	/**
 	 * action_route()
 	 */
-	public function action_route($id = null)
+	public function action_route($id)
 	{
-		is_null($id) and \Response::redirect(\Uri::base());
-
 		// model and view
 		$view = \View::forge('wrkflw/route');
 		$model_name = str_replace('Controller', 'Model', get_called_class());
 		$model = $model_name::forge();
 
+		// $obj
+		$obj = $model::find($id);
+		if ( ! $obj) 
+		{
+			\Session::set_flash('error', 'ルート設定すべき項目が見つかりませんでした。');
+			return \Response::redirect(static::$main_url);
+		}
+
 		// postがあったら経路設定して、表示画面に戻る
-		if (\Input::method() == 'POST'):
-			$route_id = \Input::post('route');
-			if ($route_id):
-				$model::set_route($route_id, \Request::active()->controller, $id);
-				\Session::set_flash('success', 'ルートを設定しました');
-
-				// update workflow_status
-				$obj = $model::find($id);
-				$obj->workflow_status = 'init';
-				$obj->save();
-
-				// ルート設定したら編集画面に返す
-				return \Response::redirect(\Uri::create(\Inflector::ctrl_to_dir(\Request::active()->controller.'/edit/'.$id)));
-			else:
-				\Session::set_flash('error', 'ルートを選択してください');
-			endif;
-		endif;
+		if (\Input::method() == 'POST')
+		{
+			if ( ! \Security::check_token())
+			{
+				\Session::set_flash('error', 'ワンタイムトークンが失効しています。送信し直してみてください。');
+			} else {
+				$route_id = \Input::post('route');
+				if ($route_id)
+				{
+					$model::set_route($route_id, \Request::active()->controller, $id);
+					\Session::set_flash('success', 'ルートを設定しました');
+	
+					// update workflow_status
+					$obj->workflow_status = 'init';
+					$obj->save();
+	
+					// ルート設定したら編集画面に返す
+					return \Response::redirect(static::$base_url.'edit/'.$id);
+				} else {
+					\Session::set_flash('error', 'ルートを選択してください');
+				}
+			}
+		}
 
 		// 設定されている経路をすべて取得
 		$model_wfadmin = \Model_Wrkflwadmin::forge();
@@ -85,14 +97,14 @@ trait Controller_Traits_Wrkflw
 		$route_id = $model::get_route(\Request::active()->controller, $id);
 
 		// add_actionset - back to edit
-		$ctrl_url = \Inflector::ctrl_to_dir(\Request::active()->controller);
-		$action['urls'][] = \Html::anchor($ctrl_url.DS.'edit/'.$id,'戻る');
+		$action['urls'][] = \Html::anchor(static::$base_url.'edit/'.$id,'戻る');
 		$action['order'] = 10;
 		\Actionset::add_actionset(\Request::active()->controller, 'ctrl', $action);
 
 		// assign
 		$view->set_global('title', 'ルート設定');
-		$view->set('button', '申請する');
+		$view->set('obj', $obj);
+		$view->set('button', '申請');
 		$view->set('items', $items);
 		$view->set('route_id', $route_id);
 		$view->set('item_id', $id);
@@ -101,27 +113,29 @@ trait Controller_Traits_Wrkflw
 
 	/**
 	 * action_apply()
+	 * 執筆者による承認申請
 	 */
-	public function action_apply($id = null)
+	public function action_apply($id)
 	{
-		is_null($id) and \Response::redirect(\Uri::base());
-
-		$controller = get_called_class();
+		$controller = \Request::active()->controller;
 		$model_name = str_replace('Controller', 'Model', $controller);
 		$model = $model_name::forge();
-		$redirect = \Inflector::ctrl_to_dir($controller);
 
 		// route_idがないということは経路設定前なので、経路設定にリダイレクト
-		$route_id = $model::get_route(\Request::active()->controller, $id);
+		$route_id = $model::get_route($controller, $id);
 		if ( ! $route_id)
 		{
 			\Session::set_flash('error', '承認申請するためには、経路設定をしてください。');
-			return \Response::redirect(\Uri::create($redirect.'/route/'.$id));
+			return \Response::redirect(static::$base_url.'route/'.$id);
 		}
 
 		// postがあったら申請処理をして、編集画面に戻る
-		if (\Input::method() == 'POST'):
-			if (\Security::check_token()):
+		if (\Input::method() == 'POST')
+		{
+			if ( ! \Security::check_token())
+			{
+				\Session::set_flash('error', 'ワンタイムトークンが失効しています。送信し直してみてください。');
+			} else {
 				$comment = \Input::post('comment');
 				$model::add_log('approve', null, $controller, $id, $comment);
 				\Session::set_flash('success', '申請しました');
@@ -131,11 +145,9 @@ trait Controller_Traits_Wrkflw
 				$obj = $target_model::find($id);
 				$obj->workflow_status = 'in_progress';
 				$obj->save();
-				return \Response::redirect(\Uri::create($redirect.'/view/'.$id));
-			else:
-				\Session::set_flash('error', 'ワンタイムトークンが失効しています。送信し直してみてください。');
-			endif;
-		endif;
+				return \Response::redirect(static::$base_url.'view/'.$id);
+			}
+		}
 
 		// コメント入力viewを表示
 		$view = \View::forge('wrkflw/comment');
@@ -147,113 +159,120 @@ trait Controller_Traits_Wrkflw
 		\Actionset::add_actionset($controller, 'ctrl', $action);
 
 		// assign
+		static::set_route_info($view, $controller, $id);
 		$view->set_global('title', '承認申請');
-		$view->set('button', '申請する');
-		$view->set('id', $id);
+		$view->set('button', '申請');
 		$this->template->content = $view;
 	}
 
 	/**
 	 * action_approve()
+	 * 承認
 	 */
-	public function action_approve($id = null)
+	public function action_approve($id)
 	{
-		$model = $this->model_name ;
-		is_null($id) and \Response::redirect(\Uri::base());
-
 		// model and view
+		$model = $this->model_name ;
 		$view = \View::forge('wrkflw/comment');
 		$model_name = str_replace('Controller', 'Model', get_called_class());
 		$model = $model_name::forge();
-
-		// redirect
-		$redirect = \Inflector::ctrl_to_dir(\Request::active()->controller);
+		$controller = \Request::active()->controller;
 
 		// postがあったら承認処理をして、閲覧画面に戻る
-		if (\Input::method() == 'POST'):
-			if (\Security::check_token()):
-				$route_id = $model::get_route(\Request::active()->controller, $id);
-				if ($route_id):
+		if (\Input::method() == 'POST')
+		{
+			if ( ! \Security::check_token())
+			{
+				\Session::set_flash('error', 'ワンタイムトークンが失効しています。送信し直してみてください。');
+			} else {
+				$route_id = $model::get_route($controller, $id);
+				if ($route_id)
+				{
 					$comment = \Input::post('comment');
 	
 					// 最後の承認かどうか確認する
-					$current_step = $model::get_current_step(\Request::active()->controller, $id) + 1;
+					$current_step = $model::get_current_step($controller, $id) + 1;
 					$total_step   = $model::get_total_step($route_id);
 					$mode = $current_step == $total_step ? 'finish' : 'approve';
 	
 					// add_log
-					$model::add_log($mode, $route_id, \Request::active()->controller, $id,$comment);
+					$model::add_log($mode, $route_id, $controller, $id,$comment);
 	
 					// 最後の承認であれば、項目のステータスを変更する
-					if ($mode == 'finish'):
+					if ($mode == 'finish')
+					{
 						$target_model = $this->model_name ;
 						$obj = $target_model::find($id);
 						$obj->workflow_status = 'finish';
 						$obj->save();
 						\Session::set_flash('success', '最終の承認をしました');
-					else:
+					} else {
 						\Session::set_flash('success', '承認しました');
-					endif;
+					}
 	
-					return \Response::redirect(\Uri::create($redirect.'/view/'.$id));
-				else:
+					return \Response::redirect(static::$base_url.'view/'.$id);
+				} else {
 					\Session::set_flash('error', '承認ルートを見つけられませんでした。');
-				endif;
-			else:
-				\Session::set_flash('error', 'ワンタイムトークンが失効しています。送信し直してみてください。');
-			endif;
-		endif;
+				}
+			}
+		}
 
 		// assign
+		static::set_route_info($view, $controller, $id);
 		$view->set_global('title', '承認');
-		$view->set('button', '承認する');
-		$view->set('id', $id);
+		$view->set('button', '承認');
 		$this->template->content = $view;
 	}
 
 	/**
 	 * action_remand()
+	 * 差し戻し
 	 */
-	public function action_remand($id = null)
+	public function action_remand($id)
 	{
-		is_null($id) and \Response::redirect(\Uri::base());
-
+		$controller = \Request::active()->controller;
 		$model_name = str_replace('Controller', 'Model', get_called_class());
 		$model = $model_name::forge();
 
 		// postがあったら差し戻し処理をして、閲覧画面に戻る
-		if (\Input::method() == 'POST'):
-			$comment     = \Input::post('comment');
-			$target_step = (int) \Input::post('target_step');
-			$model::add_log('remand', null, \Request::active()->controller, $id, $comment, $target_step);
-			\Session::set_flash('success', '差し戻し処理をしました');
-
-			// 差し戻しが最初まで戻った場合、in_progressを解除して編集できるようにする
-			if ($target_step == -1):
-				$target_model = $this->model_name ;
-				$obj = $target_model::find($id);
-				$obj->workflow_status = 'before_progress';
-				$obj->save();
-			endif;
-			
-			$redirect = \Uri::create(\Inflector::ctrl_to_dir(\Request::active()->controller));
-			return \Response::redirect($redirect);
-		endif;
+		if (\Input::method() == 'POST')
+		{
+			if ( ! \Security::check_token())
+			{
+				\Session::set_flash('error', 'ワンタイムトークンが失効しています。送信し直してみてください。');
+			} else {
+				$comment     = \Input::post('comment');
+				$target_step = (int) \Input::post('target_step');
+				$model::add_log('remand', null, $controller, $id, $comment, $target_step);
+				\Session::set_flash('success', '差し戻し処理をしました');
+	
+				// 差し戻しが最初まで戻った場合、in_progressを解除して編集できるようにする
+				if ($target_step == -1)
+				{
+					$target_model = $this->model_name ;
+					$obj = $target_model::find($id);
+					$obj->workflow_status = 'before_progress';
+					$obj->save();
+				}
+				return \Response::redirect(static::$main_url);
+			}
+		}
 
 		// 差し戻し候補を取得する
-		$current_step = $model::get_current_step(\Request::active()->controller, $id);
-		$logs         = $model::get_route_logs(\Request::active()->controller, $id, $current_step);
+		$current_step = $model::get_current_step($controller, $id);
+		$logs         = $model::get_route_logs($controller, $id, $current_step);
 		$target_steps = array();
 
 		// 名前表示用にステップを取得
-		$route_id = $model::get_route(\Request::active()->controller, $id);
+		$route_id = $model::get_route($controller, $id);
 		$q = \DB::select('*');
 		$q->from('lcm_wrkflw_steps');
 		$q->where('workflow_id', $route_id);
 		$q->order_by('seq', 'ASC');
 		$steps = $q->execute()->as_array('seq');
 
-		foreach($logs as $log):
+		foreach($logs as $log)
+		{
 			// 初期化と承認のログをとる
 			if ($log->status == 'remand') continue;
 
@@ -269,74 +288,88 @@ trait Controller_Traits_Wrkflw
 			// 複数回の差戻しによって同じステップ数を持つ場合は、keyで上書きする
 			$user_info = \Model_Usr::find($log->did_user_id);
 			$target_steps[$target_step] = $user_info->username." ({$step_name}の段階)";
-		endforeach;
+		}
 
 		// コメント入力viewを表示
 		$view = \View::forge('wrkflw/comment');
 
 		// assign
+		static::set_route_info($view, $controller, $id);
 		$view->set_global('title', '差し戻し');
-		$view->set('button',       '差し戻す');
+		$view->set('button', '差し戻し');
 		$view->set('target_steps', $target_steps);
-		$view->set('id',           $id);
 		$this->template->content = $view;
 	}
 
 	/**
 	 * action_reject()
+	 * 却下（delete）
 	 */
-	public function action_reject($id = null)
+	public function action_reject($id)
 	{
-		is_null($id) and \Response::redirect(\Uri::base());
-
 		// model and view
+		$controller = \Request::active()->controller;
 		$view = \View::forge('wrkflw/comment');
 		$model_name = str_replace('Controller', 'Model', get_called_class());
 		$model = $model_name::forge();
 
 		// postがあったら経路設定して、編集画面に戻る
-		if (\Input::method() == 'POST'):
-			$route_id = $model::get_route(\Request::active()->controller, $id);
-
-			if ($route_id):
-				$comment = \Input::post('comment');
-				$model::add_log('reject', $route_id, \Request::active()->controller, $id, $comment);
-
-				$controller = \Request::active()->controller;
-
-echo '<textarea style="width:100%;height:200px;background-color:#fff;color:#111;font-size:90%;font-family:monospace;position:relative;z-index:9999">' ;
-var_dump( $controller ) ;
-echo '</textarea>' ;
-die();
-
-
-				$redirect = \Inflector::ctrl_to_dir(\Request::active()->controller);
-
-				// 項目を削除する（可能であればソフトデリートする）
-				$target_model = $this->model_name ;
-				$obj = $target_model::find($id);
-				if ( ! $obj)
+		if (\Input::method() == 'POST')
+		{
+			if ( ! \Security::check_token())
+			{
+				\Session::set_flash('error', 'ワンタイムトークンが失効しています。送信し直してみてください。');
+			} else {
+				$route_id = $model::get_route($controller, $id);
+	
+				if ($route_id)
 				{
-					\Session::set_flash('success', '存在しない項目を却下しようとしました。');
-					return \Response::redirect(\Uri::create($redirect));
-				}
+					$comment = \Input::post('comment');
+					$model::add_log('reject', $route_id, $controller, $id, $comment);
 
-				if (isset($obj::properties()['deleted_at']))
-				{
-					$obj->delete();
-				} else {
-					$obj->purge(null, true);
+					// 項目を削除する（可能であればソフトデリートする）
+					$target_model = $this->model_name ;
+					$obj = $target_model::find($id);
+					if ( ! $obj)
+					{
+						\Session::set_flash('success', '存在しない項目を却下しようとしました。');
+						return \Response::redirect(static::$main_url);
+					}
+	
+					if (isset($obj::properties()['deleted_at']))
+					{
+						$obj->delete();
+					} else {
+						$obj->purge(null, true);
+					}
+					\Session::set_flash('success', '項目を却下しました。');
+	
+					return \Response::redirect(static::$main_url);
 				}
-				\Session::set_flash('success', '項目を却下しました');
-
-				return \Response::redirect(\Uri::create($redirect));
-			endif;
-		endif;
+			}
+		}
 
 		// assign
+		static::set_route_info($view, $controller, $id);
 		$view->set_global('title', '却下の確認');
-		$view->set('button', '却下する');
-		$view->set('id', $id);
+		$view->set('button', '却下');
 		$this->template->content = $view;
+	}
+
+	/**
+	 * set_route_info()
+	 * どのアクションでも使うのでメンバ変数に持たせる
+	 */
+	public function set_route_info($view, $controller, $obj_id)
+	{
+		$model = \Inflector::maybe_ctrl_to_model($controller);
+		$route_id = $model::get_route($controller, $obj_id); // AKA workflow_id
+//		$logs     = $model::get_strait_route_logs($controller, $obj_id);
+
+		// assign
+		$view->set('id', $obj_id);
+		$view->set('current_step', $model::get_current_step($controller, $obj_id) + 1);
+		$view->set('total_step', $model::get_total_step($route_id));
+		$view->set('workflow', \Model_Wrkflwadmin::find($route_id));
 	}
 }
