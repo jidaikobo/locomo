f30fefaa7dc01c4c6d51b49e66b78a5ef3e8adb6
shibata <shibata@jidaikobo.com>
2015-02-07 02:31:09 +0900

    ツールバーのコントローラトップへのリンク修正。

--- views/inc_admin_bar.php
+++ views/inc_admin_bar.php
@@ -25,14 +25,15 @@ if (\Auth::check()):
 				$ctrl_index = '';
 				// Controller_Sys
 				if ($current_name == '-Controller_Sys'):
-					if(\Auth::has_access(\Request::main()->controller.'::home')):
+					if(\Auth::has_access($locomo['controller']['name'].'::home')):
 						$top_link = \Html::anchor(\Uri::create('sys/admin/'), '管理トップ');
 					else:
 						$top_link = '管理トップ';
 					endif;
 				else:
-//					if(\Auth::has_access(\Request::main()->controller.DS.'sys/admin')):
-					if(\Auth::has_access(\Request::main()->controller.'::'.\Arr::get($locomo, 'controller.main_action'))):
+//					if(\Auth::has_access($locomo['controller']['name'].DS.'sys/admin')):
+					// 権限があればコントローラトップへのリンクを表示
+					if(\Auth::has_access($locomo['controller']['name'].'::'.\Arr::get($locomo, 'controller.main_action'))):
 						$top_link = \Html::anchor(\Uri::create('sys/admin/'.$current_name), $current_nicename).' ';
 					else:
 						$top_link = $current_nicename;
