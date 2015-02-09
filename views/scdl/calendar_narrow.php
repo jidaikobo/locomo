553c2809419c712745e5a764940e1b236745794e
清水 琢 <shimizutaku@shimizu-mac.local>
2015-02-09 23:53:26 +0900

    onchangeからボタンへ

--- views/scdl/calendar_narrow.php
+++ views/scdl/calendar_narrow.php
@@ -1,14 +1,16 @@
 
-<select id="narrow_user_group_id" onchange="location.href= '?ugid=' + this.value">
+<select class="schedule_narrow" id="narrow_user_group_id" >
 	<option value="-1">--- 全て ---
 	<?php foreach($narrow_user_group_list as $key => $value) { ?>
 		<option value="<?php print $key; ?>" <?php if ($key == \Session::get($model_name . "narrow_ugid")) { print "selected"; } ?>><?php  print $value; ?>
 	<?php } ?>
 </select>
-<select id="narrow_user_id" name="narrow_user_list" onchange="location.href='?uid=' + this.value">
+
+<input class="schedule_narrow" id="btn_user_group" type="button" value="絞り込み" onclick="javascript:location.href='?ugid=' + $('#narrow_user_group_id').val()" />
+<select class="schedule_narrow" id="narrow_user_id" name="narrow_user_list" >
 	<option value="">--- 全て ---
 	<?php foreach($narrow_user_list as $row) { ?>
 		<option value="<?php print $row['id']; ?>" <?php if ($row['id'] == \Session::get($model_name . "narrow_uid")) { print "selected"; } ?>><?php  print $row['display_name']; ?>
 	<?php } ?>
 </select>
-
+<input class="schedule_narrow" id="btn_user" type="button" value="絞り込み" onclick="javascript:location.href='?uid=' + $('#narrow_user_id').val()" />
