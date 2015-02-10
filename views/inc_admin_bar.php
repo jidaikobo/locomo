4bf0614b81d2f9bc070f8cbcb8980b39594cfdf0
有松 <arimatsu@jidaikobo.com>
2015-02-10 20:15:43 +0900

    スケジューラ周りのレイアウト等、すこし変更しました

--- views/inc_admin_bar.php
+++ views/inc_admin_bar.php
@@ -155,7 +155,7 @@ if (\Auth::check()):
 			$html.= '<li class="show_if_smalldisplay"><span class="label">'.\Auth::get('display_name').'</span></li>';
 			$html.= "<li><a href=\"".\Uri::base()."auth/logout\">ログアウト</a></li>";
 			if ( ! \Auth::is_admin()):
-				$html.= "<li class=\"has_top_separator\"><a href=\"".\Uri::base()."usr/view/".\Auth::get('id')."\">ユーザ情報</a></li>";
+				$html.= "<li class=\"has_bottm_separator\"><a href=\"".\Uri::base()."usr/view/".\Auth::get('id')."\">ユーザ情報</a></li>";
 			endif;
 			// usergroup
 			$usergroups = \Auth::get('usergroup');
--- views/scdl/calendar_narrow.php
+++ views/scdl/calendar_narrow.php
@@ -1,16 +1,16 @@
 
-ユーザーグループ<select class="schedule_narrow" id="narrow_user_group_id" >
-	<option value="-1">--- 全て ---
+<select class="schedule_narrow" id="narrow_user_group_id" title="ユーザーグループ">
+	<option value="-1">-- ユーザーグループ --
 	<?php foreach($narrow_user_group_list as $key => $value) { ?>
 		<option value="<?php print $key; ?>" <?php if ($key == \Session::get($model_name . "narrow_ugid")) { print "selected"; } ?>><?php  print $value; ?>
 	<?php } ?>
 </select>
+<input class="schedule_narrow button small" id="btn_user_group" type="button" value="絞り込み" onclick="javascript:location.href='?ugid=' + $('#narrow_user_group_id').val()" style="margin-right: 1em;" />
 
-<input class="schedule_narrow" id="btn_user_group" type="button" value="絞り込み" onclick="javascript:location.href='?ugid=' + $('#narrow_user_group_id').val()" />
-ユーザー<select class="schedule_narrow" id="narrow_user_id" name="narrow_user_list" >
-	<option value="">--- 全て ---
+<select class="schedule_narrow" id="narrow_user_id" name="narrow_user_list" title="ユーザー">
+	<option value="">-- ユーザー --
 	<?php foreach($narrow_user_list as $row) { ?>
 		<option value="<?php print $row['id']; ?>" <?php if ($row['id'] == \Session::get($model_name . "narrow_uid")) { print "selected"; } ?>><?php  print $row['display_name']; ?>
 	<?php } ?>
 </select>
-<input class="schedule_narrow" id="btn_user" type="button" value="絞り込み" onclick="javascript:location.href='?uid=' + $('#narrow_user_id').val()" />
+<input class="schedule_narrow button small" id="btn_user" type="button" value="絞り込み" onclick="javascript:location.href='?uid=' + $('#narrow_user_id').val()" />
