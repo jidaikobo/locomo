a8e9daf39894af252c2d839fab161a1fb5104c60
有松 <arimatsu@jidaikobo.com>
2015-02-16 16:03:05 +0900

    ファイラの権限設定で、親権限がない時の状況に対応

--- classes/model/flr.php
+++ classes/model/flr.php
@@ -797,7 +797,7 @@ class Model_Flr extends \Model_Base
 			$g_permissions = array();
 			foreach ($parent->permission_usergroup as $k => $v)
 			{
-				if ( ! isset($v->usrgrp)) continue;
+				if ( ! is_object($v->usrgrp)) continue;
 				$g_permissions[$v->usergroup_id] = $v->usrgrp->name;
 			}
 			$options = array_intersect($g_permissions, $options);
@@ -822,7 +822,7 @@ class Model_Flr extends \Model_Base
 			$u_permissions = array();
 			foreach ($parent->permission_user as $k => $v)
 			{
-				if ( ! isset($v->usr)) continue;
+				if ( ! is_object($v->usr)) continue;
 				$g_permissions[$v->user_id] = $v->usr->display_name;
 			}
 			$options = array_intersect($u_permissions, $options);
