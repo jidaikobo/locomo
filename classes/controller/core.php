0c13fd447a85aaa861e3bda7b206b3b07cadd383
shibata <shibata@jidaikobo.com>
2015-02-19 23:33:13 +0900

    IE8で、code profilerが悪さをするので非表示

--- classes/controller/core.php
+++ classes/controller/core.php
@@ -47,6 +47,7 @@ class Controller_Core extends \Fuel\Core\Controller_Rest
 		// show profile to development only
 		\Fuel::$profiling = \Fuel::$env == 'development' ?: false ;
 		\Fuel::$profiling = \Input::get('no_prof') ? false : \Fuel::$profiling ;
+		\Fuel::$profiling = \Locomo\Browser::getIEVersion() && \Locomo\Browser::getIEVersion() <= 8 ? false : \Fuel::$profiling;
 
 		// hmvc
 		$this->_template = \Request::is_hmvc() ? 'widget' : $this->_template ;
