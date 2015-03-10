fb28f59dd99f60468ee295c444ed6d66aed32402
有松 <arimatsu@jidaikobo.com>
2015-03-10 16:04:03 +0900

    週間カレンダの昨日分修正

--- views/scdl/calendar_week_building.php
+++ views/scdl/calendar_week_building.php
@@ -22,27 +22,27 @@
 <table class="calendar week lcm_focus" title="カレンダ">
 <?php if(!\Request::is_hmvc()): ?>
 	<thead>
-		<tr>
-			<th>&nbsp;</th>
-			<th class="week1"><span>月曜日</span></th>
-			<th class="week2"><span>火曜日</span></th>
-			<th class="week3"><span>水曜日</span></th>
-			<th class="week4"><span>木曜日</span></th>
-			<th class="week5"><span>金曜日</span></th>
-			<th class="week6"><span>土曜日</span></th>
-			<th class="week0"><span>日曜日</span></th>
-		</tr>
+	<tr>
+		<th>&nbsp;</th>
+		<th class="week1"><span>月曜日</span></th>
+		<th class="week2"><span>火曜日</span></th>
+		<th class="week3"><span>水曜日</span></th>
+		<th class="week4"><span>木曜日</span></th>
+		<th class="week5"><span>金曜日</span></th>
+		<th class="week6"><span>土曜日</span></th>
+		<th class="week0"><span>日曜日</span></th>
+	</tr>
 	</thead>
 <?php endif; ?>
-<tbody>
+	<tbody>
 <?php $detail_pop_array = array(); ?>
-<?php foreach($schedule_data['building_list'] as $row) { ?>
+<?php foreach($schedule_data['building_list'] as $row): ?>
 	<tr class="lcm_focus" title="<?php echo  $row['model']->item_name ?>">
 		<th>
 		<?php print $row['model']->item_name; ?>
 		</th>
-		<?php foreach($schedule_data['schedules_list'] as $schedule_row):?>
-		<?php 
+		<?php
+		foreach($schedule_data['schedules_list'] as $schedule_row):
 			$class_str = 'week'.$schedule_row['week'];
 			$class_str.= $currentday == $schedule_row['day'] ? ' today' : '';
 //			$class_str.= $currentday ==  ? ' holiday' : ''; //祝日のとき
@@ -65,7 +65,6 @@
 //			$each_date_title_skip.=  ? '祝日</span><span class="holiday_name">'..'</span><span class="skip">' : '';
 			$each_date_title_skip.= $schedule_num!=0 ? ' '.$schedule_num . '件の登録' : ' 登録なし';
  ?>
-		
 		<td class="<?php print $class_str; ?>">
 			<div class="each_date lcm_focus" title="<?php echo $each_date_title_str ?>">
 				<a href="<?php echo \Uri::create(Config::get('base_url') . $kind_name . '/calendar/' . sprintf("%04d/%02d/%02d/", $year, $mon, $schedule_row['day'])); ?>" class="title">
@@ -73,79 +72,62 @@
 					<span class="skip"><?php print $each_date_title_skip ?></span>
 				</a>
 				<a href="<?php echo \Uri::create($kind_name . "/create?ymd=" . htmlspecialchars(sprintf("%04d-%02d-%02d", $year, $mon, $schedule_row['day']))); ?>" class="add_new" title="新規追加"><span class="skip">新規追加</span></a>
-			
 				<div class="events">
 					<?php
-						if (isset($schedule_row['day'])) { ?>
-								<?php foreach ($schedule_row['data'] as $v2) {
-									foreach ($row['data'] as $building_detail) {
-										if ($building_detail['id'] == $v2['scdlid']) {
-
-											$detail_pop_array[] = $v2;
-											?>
-											<p class="lcm_tooltip_parent" data-jslcm-tooltip-id="pop<?php echo $v2->scdlid.$v2->target_year.$v2->target_mon.$v2->target_day ?>">
-										<?php
-											//繰り返し区分
-											echo $v2['repeat_kb'] != 0 ? '<span class="text_icon schedule repeat_kb_'.$v2['repeat_kb'].'"><span class="skip"> '.$repeat_kbs[$v2['repeat_kb']].'</span></span>' : '';
-											//詳細区分
-											foreach($detail_kbs as $key => $value){
-												if($v2[$key]){
-													 echo '<span class="text_icon schedule '.$key.'"><span class="skip">'.$value.'</span></span>';
-												}
-											}
-											//重要度
-											$importance_v = $model_name::value2index('title_importance_kb', html_entity_decode($v2['title_importance_kb']));
-											echo '<span class="icon"><img src="'.\Uri::base().'lcm_assets/img/system/mark_importance_'.$importance_v.'.png" alt="'.$importance_kbs[$importance_v].'"></span>';
-											print htmlspecialchars_decode($v2['link_detail']);
-										?>
-											</p>
-
-									<?php }
-										}
-									}
-								}
-							
+					if (isset($schedule_row['day'])):
+						foreach ($schedule_row['data'] as $v2):
+							foreach ($row['data'] as $building_detail):
+								if ($building_detail['id'] == $v2['scdlid']):
+									$detail_pop_array[] = $v2;
+									echo '<p class="lcm_tooltip_parent" data-jslcm-tooltip-id="pop'.$v2->scdlid.$v2->target_year.$v2->target_mon.$v2->target_day.'">';
+									//繰り返し区分
+									echo $v2['repeat_kb'] != 0 ? '<span class="text_icon schedule repeat_kb_'.$v2['repeat_kb'].'"><span class="skip"> '.$repeat_kbs[$v2['repeat_kb']].'</span></span>' : '';
+									//詳細区分
+									foreach($detail_kbs as $key => $value):
+										if($v2[$key]):
+											echo '<span class="text_icon schedule '.$key.'"><span class="skip">'.$value.'</span></span>';
+										endif;
+									endforeach;
+									//重要度
+									$importance_v = $model_name::value2index('title_importance_kb', html_entity_decode($v2['title_importance_kb']));
+									echo '<span class="icon"><img src="'.\Uri::base().'lcm_assets/img/system/mark_importance_'.$importance_v.'.png" alt="'.$importance_kbs[$importance_v].'"></span>';
+									print htmlspecialchars_decode($v2['link_detail']);
+									echo '</p>';
+								endif;
+							endforeach;
+						endforeach;
+					endif;
 					?>
 				</div><!-- /.events -->
 			</div><!-- /.each_date -->
 		</td>
-<?php
-	endforeach;
-	print "</tr>";
-}
-?>
-
-
-
-
-
-	
-
-</tr>
-</tbody>
+<?php	endforeach;?>
+	</tr>
+<?php endforeach; ?>
+	</tbody>
 </table>
 <?php if(!\Request::is_hmvc()): ?>
 </div><!-- /.field_wrapper.calendar -->
 <?php endif; ?>
-	<div class="legend calendar">
-<?php
-	foreach($repeat_kbs as $k => $v){
+<?php //カレンダ凡例
+	echo '<div class="legend calendar">';
+	foreach($repeat_kbs as $k => $v):
 		echo $k != 0 ? '<span class="display_inline_block"><span class="text_icon schedule repeat_kb_'.$k.'"><span class="skip"> '.$v.'</span></span>'.$v.' </span>' : '';
-	}
-	foreach($detail_kbs as $k => $v){
+	endforeach;
+	foreach($detail_kbs as $k => $v):
 		echo $k != 'unspecified_kb' ? '<span class="display_inline_block"><span class="text_icon schedule '.$k.'"><span class="skip"> '.$v.'</span></span>'.$v.' </span>' : '';
-	}
+	endforeach;
 	if(!\Request::is_hmvc()): //重要度
-		foreach($importance_kbs as $k => $v){
+		foreach($importance_kbs as $k => $v):
 			echo '<span class="display_inline_block"><span class="icon mark_importance"><img src="'.\Uri::base().'lcm_assets/img/system/mark_importance_'.$k.'.png" alt="'.$v.'"></span>'.$v.'</span>';
-		}
+		endforeach;
 	endif;
 //	echo $locomo['controller']['name'] === "\Controller_Scdl" ? '<span class="display_inline_block"><span class="icon mark_private"><img src="'.Uri::base().'lcm_assets/img/system/mark_private.png" alt="非公開"></span>非公開</span>' : '';
+	echo '</div><!-- /.legend.calendar -->';
 ?>
-	 </div><!-- /.legend.calendar -->
-<?php foreach($detail_pop_array as $v) {
-	$detail_pop_data = $v;
-	include("detail_pop.php");
-} ?>
-
-
+<?php //ポップアップ読み込み
+	foreach($detail_pop_array as $v):
+		$detail_pop_data = $v;
+		include("detail_pop.php");
+	endforeach;
+?>
