79824d5e2da36dbcd3144c5ff617e9bb4a4f2d94
yamamorin <yamamori@studio-nicr.com>
2015-03-18 15:28:58 +0900

    ヘルプファイル追加。

---
+++ classes/help/msgbrd.php
@@ -0,0 +1,19 @@
+<?php
+namespace Locomo;
+class Help_Scdl
+{
+// index_admin
+static $index_admin = <<< EOF
+
+EOF;
+
+// index_draft
+static $index_draft = <<< EOF
+
+EOF;
+
+// edit_categories
+static $edit_categories = <<< EOF
+
+EOF;
+}
