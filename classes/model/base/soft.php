b84f0e8f6907b466e1d31aaaca26461f6810c189
shibata <shibata@jidaikobo.com>
2015-04-14 19:57:01 +0900

    model_softを修正

--- classes/model/base/soft.php
+++ classes/model/base/soft.php
@@ -2,6 +2,8 @@
 namespace Locomo;
 class Model_Base_Soft extends \Orm\Model_Soft
 {
+	use \Model_Traits_Base;
+
 	protected static $_soft_delete = array(
 		'deleted_field'   => 'deleted_at',
 		'mysql_timestamp' => true,
