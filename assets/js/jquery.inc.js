0b100a411ee471fffd0392ba8ffb9bbc576932de
有松 <arimatsu@jidaikobo.com>
2015-09-16 17:10:27 +0900

    tinymce周りの設定追加

--- assets/js/jquery.inc.js
+++ assets/js/jquery.inc.js
@@ -1155,24 +1155,51 @@ if($('#schedule_graph')[0]){
 /* Tiny MCE  */
 tinymce.init({
 	mode : "none",
-//	selector: "textarea.tinymce",
+	resize: "both",
 	theme : "modern",
+	language: 'ja',
 	theme_advanced_buttons3_add : "tablecontrols",
-	plugins:"table code"	
+	plugins:"table code",
 });
+
 $(':input.tinymce').each(function(){
-	var id = this.id;
-	$(this).before('<p class="cf" style="font-size:.8em;"><a id="switch_'+id+'" class="switch_mce is_text" href="javascript: void(0);">')
-	$(document).find('.switch_mce').text('ビジュアルエディタを使用');
+	var id, state, label, btn;
+	id = this.id;
+	if($(this).hasClass('on')){
+		state = 'on';
+		label = 'テキストエディタに切替え';
+		tinymce.EditorManager.execCommand('mceAddEditor', true, id);
+	}else{
+		state = 'off';
+		label = 'ビジュアルエディタに切替え';	
+	}
+	if($(this).hasClass('nolabel')) {
+		state += ' nolabel';
+	}
+	$(this).before('<p class="cf" style="font-size:.8em;"><a id="switch_'+id+'" class="switch_mce '+state+'" href="javascript: void(0);">')
+	btn = $(this).prev().find('.switch_mce');
+	if($(this).hasClass('nolabel')){
+		btn.html('<span class="skip">'+label+'</span>');
+	}else{
+		btn.text(label);
+	}
 });
 $(document).on('click', '.switch_mce', function(){
-	var id = this.id.replace('switch_','');
-	if( $(this).hasClass('is_visual') ){
-		$(this).removeClass('is_visual').addClass('is_text').text('ビジュアルエディタを使用');
+	var id, label;
+	id = this.id.replace('switch_','');
+	if( $(this).hasClass('on') ){
+		$(this).removeClass('on').addClass('off');
+		label = 'ビジュアルエディタに切替え';
 		tinymce.EditorManager.execCommand('mceRemoveEditor', false, id);
 	} else {
-		$(this).removeClass('is_text').addClass('is_visual').text('テキストエディタを使用');
+		$(this).removeClass('off').addClass('on');
+		label = 'テキストエディタに切替え';
 		tinymce.EditorManager.execCommand('mceAddEditor', true, id);
+	}
+	if($(this).hasClass('nolabel')){
+		$(this).html('<span class="skip">'+label+'</span>');
+	}else{
+		$(this).text(label);
 	}
 });
 
