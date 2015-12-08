6f2790cbc9b4e7ac0e13f5fa632849a2b5eb984a
shibata <shibata@jidaikobo.com>
2015-12-08 21:31:15 +0900

    公開側indexにもset_search_options()を足した。多分問題はないし、この使い方の方が多いと思う。

--- classes/controller/base.php
+++ classes/controller/base.php
@@ -14,6 +14,7 @@ class Controller_Base extends Controller_Core
 
 		// set options
 		$model::set_public_options();
+		$model::set_search_options();
 		$model::set_paginated_options();
 
 		// find()
