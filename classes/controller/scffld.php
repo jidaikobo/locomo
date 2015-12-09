3c8fe1b0109b2f4e92e45b58a7fd5507d9e0ee2b
shibata <shibata@jidaikobo.com>
2015-12-09 21:15:44 +0900

    スキャフォールドの開発中由来のバグ修正

--- classes/controller/scffld.php
+++ classes/controller/scffld.php
@@ -255,7 +255,7 @@ class Controller_Scffld extends \Controller_Base
 			if ( ! file_exists($log_dir)) \File::create_dir(APPPATH.'logs', 'scffld/'.$name);
 			$latest = \Util::get_latestprefix($log_dir);
 			\File::update($log_dir, $latest.'_scaffold.txt', $cmd_raw);
-			\File::update($log_dir, 'files.php', "<?php\n$scfflds = ".var_export($files, 1).';');
+			\File::update($log_dir, 'files.php', '<?php'."\n".'$scfflds = '.var_export($files, 1).';');
 
 			// message
 			$messages[] = "{$log_dir}";
