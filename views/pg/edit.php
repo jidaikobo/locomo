796c432babffe56afd55105102d4ffe1c3dd39aa
shibata <shibata@jidaikobo.com>
2016-06-17 18:00:12 +0900

    編集ボタンを新式に統一した

--- views/pg/edit.php
+++ views/pg/edit.php
@@ -10,74 +10,6 @@ echo \Form::open(array('class' => 'lcm_form form_group'));
 	echo $form;
 ?>
 
-<?php /* ?>
-<table class="formtable">
-<tr>
-	<th><?php echo $form->field('title')->set_template('{label}{required}'); ?></th>
-	<td><?php echo $form->field('title')->set_template('{error_msg}{field}'); ?></td>
-</tr>
-
-<tr>
-	<th><?php echo $form->field('path')->set_template('{label}{required}'); ?></th>
-	<td><?php echo $form->field('path')->set_template('{error_msg}{field}'); ?></td>
-</tr>
-
-<tr>
-	<th><?php echo $form->field('url')->set_template('{label}{required}'); ?></th>
-	<td><?php echo $form->field('url')->set_template('{error_msg}{field}'); ?></td>
-</tr>
-
-<tr>
-	<th><?php echo $form->field('summary')->set_template('{label}{required}'); ?></th>
-	<td><?php echo $form->field('summary')->set_template('{error_msg}{field}'); ?></td>
-</tr>
-
-<tr>
-	<th><?php echo $form->field('content')->set_template('{label}{required}'); ?></th>
-	<td><?php echo $form->field('content')->set_template('{error_msg}{field}'); ?></td>
-</tr>
-
-<tr>
-	<th><?php echo $form->field('lat')->set_template('{label}{required}'); ?></th>
-	<td><?php echo $form->field('lat')->set_template('{error_msg}{field}'); ?></td>
-</tr>
-
-<tr>
-	<th><?php echo $form->field('lng')->set_template('{label}{required}'); ?></th>
-	<td><?php echo $form->field('lng')->set_template('{error_msg}{field}'); ?></td>
-</tr>
-
-<tr>
-	<th><?php echo $form->field('created_at')->set_template('{label}{required}'); ?></th>
-	<td><?php echo $form->field('created_at')->set_template('{error_msg}{field}'); ?></td>
-</tr>
-
-<tr>
-	<th><?php echo $form->field('expired_at')->set_template('{label}{required}'); ?></th>
-	<td><?php echo $form->field('expired_at')->set_template('{error_msg}{field}'); ?></td>
-</tr>
-
-<tr>
-	<th><?php echo $form->field('is_sticky')->set_template('{label}{required}'); ?></th>
-	<td><?php echo $form->field('is_sticky')->set_template('{error_msg}{field}'); ?></td>
-</tr>
-
-<?php if (\Auth::is_admin()): ?>
-<tr>
-	<th><?php echo $form->field('is_visible')->set_template('{label}{required}'); ?></th>
-	<td><?php echo $form->field('is_visible')->set_template('{error_msg}{field}'); ?></td>
-</tr>
-
-<?php endif; ?>
-<tr>
-	<th><?php echo $form->field('is_available')->set_template('{label}{required}'); ?></th>
-	<td><?php echo $form->field('is_available')->set_template('{error_msg}{field}'); ?></td>
-</tr>
-
-
-</table>
-<?php */ ?>
-
 <?php
 	// revision memo template - optional
 	//echo render(LOCOMOPATH.'views/revision/inc_revision_memo.php');
@@ -92,7 +24,6 @@ endif;
 
 	if ( ! @$is_revision):
 		echo \Form::hidden(\Config::get('security.csrf_token_key'), \Security::fetch_token());
-		echo \Form::submit('submit', '保存する', array('class' => 'button primary'));
 	endif;
 	?>
 </div>
