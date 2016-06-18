3bbb03a0c58cadb49121ccaa12d19273dde528a4
shibata <shibata@jidaikobo.com>
2016-06-18 19:47:17 +0900

    routes.phpの改善

--- _to_app_dir/config/routes.php
+++ _to_app_dir/config/routes.php
@@ -53,7 +53,12 @@ $ret = array(
 			// lang
 			else
 			{
-				return \Request::forge(join('/', array_splice($segments, 2)), false)->execute();
+				try {
+					return \Request::forge(join('/', array_splice($segments, 2)), false)->execute();
+				} catch (\HttpNotFoundException $e)
+				{
+					return \Request::forge('sys/404', false)->execute();
+				}
 			}
 		}
 		// 404
@@ -62,19 +67,3 @@ $ret = array(
 );
 
 return $ret;
-
-/*
-return array(
-	'_root_'  => 'sys/home',
-	'_404_'   => 'sys/404',
-
-	// pg
-	'pg/create'               => 'pg/create',
-	'pg/index_(:any)'         => 'pg/index_$1',
-	'pg/edit/(:num)'          => 'pg/edit/$1',
-	'pg/delete/(:num)'        => 'pg/delete/$1',
-	'pg/purge_confirm/(:num)' => 'pg/purge_confirm/$1',
-	'pg/purge/(:num)'         => 'pg/purge/$1',
-	'pg/(:any)'               => 'pg/view/$1',
-);
-*/
